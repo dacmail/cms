afd5358bf5566be33647fa6270a2706edf02ec62
Jaime Sares <jaimesares@gmail.com>
2017-01-15 22:45:56 +0100

    Añadido el middleware para la sección superadmin

--- app/Http/Middleware/CheckSuperAdmin.php
+++ app/Http/Middleware/CheckSuperAdmin.php
@@ -15,6 +15,10 @@ class CheckSuperAdmin
      */
     public function handle($request, Closure $next)
     {
+        if (! $request->user() || $request->user() && $request->user()->web->subdomain !== 'admin') {
+            abort(401);
+        }
+
         return $next($request);
     }
 }
