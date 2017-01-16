1131329140e9ab131a8187b688c8d3bf3739977a
Jaime Sares <jaimesares@gmail.com>
2017-01-16 19:23:38 +0100

    Finalizado el script protecms:seeddevdata

--- database/seeds/DatabaseSeeder.php
+++ database/seeds/DatabaseSeeder.php
@@ -24,55 +24,7 @@ class DatabaseSeeder extends Seeder
             }
         }
 
-        $permissions = [
-            'admin',
-            'admin.panel.stats',
-            'admin.calendar',
-            'admin.calendar.view',
-            'admin.finances',
-            'admin.finances.view',
-            'admin.design',
-            'admin.design.view',
-            'admin.support',
-            'admin.reports',
-            'admin.tasks',
-            'admin.tasks.view',
-            'admin.panel.posts',
-            'admin.panel.posts.view',
-            'admin.panel.posts.crud',
-            'admin.panel.pages',
-            'admin.panel.pages.view',
-            'admin.panel.pages.crud',
-            'admin.panel.forms',
-            'admin.panel.forms.view',
-            'admin.panel.forms.crud',
-            'admin.panel.files',
-            'admin.panel.files.view',
-            'admin.panel.files.crud',
-            'admin.panel.users',
-            'admin.panel.users.view',
-            'admin.panel.partners',
-            'admin.panel.partners.view',
-            'admin.panel.veterinarians',
-            'admin.panel.veterinarians.view',
-            'admin.panel.animals',
-            'admin.panel.animals.dog',
-            'admin.panel.animals.dog.view',
-            'admin.panel.animals.cat',
-            'admin.panel.animals.cat.view',
-            'admin.panel.animals.horse',
-            'admin.panel.animals.horse.view',
-            'admin.panel.animals.rodent',
-            'admin.panel.animals.rodent.view',
-            'admin.panel.animals.bird',
-            'admin.panel.animals.bird.view',
-            'admin.panel.animals.reptil',
-            'admin.panel.animals.reptil.view',
-            'admin.panel.animals.other',
-            'admin.panel.animals.other.view',
-            'admin.panel.temporaryhomes',
-            'admin.panel.temporaryhomes.view'
-        ];
+        $permissions = config('protecms.users.permissions');
 
         foreach ($permissions as $permission) {
             Permission::create([
